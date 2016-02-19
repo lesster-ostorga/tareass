2078d0d912119064ecd16cffe9df5cf21dfa7b9b
lesster-ostorga <lesster3048@hotmail.com>
2016-02-18 22:10:00 -0600

    tareaa

---
+++ trabajo/src/trabajo.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : trabajo.cpp
+// Author      : lesster
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+
+void saludar (char * Nombre) {
+cout << "hola " << Nombre << endl;
+}
+
+int main() {
+	saludar (" Lesster");
+	return 0;
+}
+
+
+//void verMensaje (const char * Mensaje)
+//{
+//cout << Mensaje << endl;
+//}
